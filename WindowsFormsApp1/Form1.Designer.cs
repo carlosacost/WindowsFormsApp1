175aa63f78471c41c617f9ad6755001c36f1c4d4
carlosacost <64041429+carlosacost@users.noreply.github.com>
2021-06-14 19:32:49 -0400

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Lexema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(375, 219);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(59, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Analizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(374, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(687, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Limpiar Tabla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(29, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ingrese el código para analizar:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Token
+            // 
+            this.Token.HeaderText = "Tokens";
+            this.Token.MinimumWidth = 6;
+            this.Token.Name = "Token";
+            this.Token.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Token.Width = 125;
+            // 
+            // Lexema
+            // 
+            this.Lexema.HeaderText = "Lexema";
+            this.Lexema.MinimumWidth = 6;
+            this.Lexema.Name = "Lexema";
+            this.Lexema.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Lexema.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Token,
+            this.Lexema});
+            this.dataGridView1.Location = new System.Drawing.Point(424, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(352, 219);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Text = "dataGridView1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Token;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lexema;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Lexico Analiza_Lexico = new Lexico();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        class Automata{
+            string _textoIma;
+            int _edoAct;
+            char SigCar(ref int i)
+            {
+                if (i == _textoIma.Length)
+                {
+                    i++;
+                    return ' ';
+
+                }
+                else
+                    return _textoIma[i++];
+            }
+             public bool Reconoce (string texto,int iniToken,ref int i,int noAuto)
+            {
+                char c;
+                _textoIma = texto;
+                string lenguaje;
+                switch (noAuto)
+                {
+                    case 0: _edoAct = 0;
+                        break;
+                        //---------Automata delimmm-----
+                    case 1: _edoAct = 3;
+                        break;
+                    //---------Automata delimmm-----
+                    case 2: _edoAct = 6;
+                        break;
+                    case 3: _edoAct = 9;
+                        break;
+                    case 4: _edoAct = 11;
+                        break;
+
+                }
+                while(i<=_textoIma.Length)
+                    switch (_edoAct)
+                    {
+                        case 0: c = SigCar(ref i);
+                            if ((lenguaje = " \n\r\t").IndexOf(c) >= 0)
+                                _edoAct=1;
+                            else
+                            {
+                                i = iniToken;
+                                return false;
+                            }
+                            break;
+                        case 1:  c= SigCar(ref i);
+                            if ((lenguaje = " \n\r\t").IndexOf(c)>= 0)
+                                _edoAct = 1;
+                            else {
+                                if ((lenguaje = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_'abcdefghijklmnopqrstuvwxyz").IndexOf(c) >= 0)
+                                    _edoAct = 2;
+                                else
+                                {
+                                    i = iniToken;
+                                    return false;
+                                } 
+                                    
+                            }break;
+                        case 2: i--;
+                            return true;
+                            break;
+                        case 3: c = SigCar(ref i);
+                            if ((lenguaje = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz").IndexOf(c) >= 0)
+                                _edoAct = 4;
+                            else
+                            {
+                                i = iniToken;
+                                return false;
+                            }break;
+                        case 4: c = SigCar(ref i);
+                            if ((lenguaje = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz").IndexOf(c) >= 0)
+                                _edoAct = 4;
+                            else  if ((lenguaje = "0123456789").IndexOf(c) >= 0)
+                                    _edoAct = 4;
+                            else  if ((lenguaje = "_").IndexOf(c) >= 0)
+                                        _edoAct = 4;
+                                            
+                            else  if ((lenguaje = "!\"#$%&\'()*+,-./:;<=>?@[\\]^`{│}~ \n\t\r\f").IndexOf(c) >= 0)
+                                        _edoAct = 5;
+                                             else { i = iniToken;
+                                    return false;
+                                }
+                                break;
+                        case 5: i--;
+                            return true;
+                            break;
+                        case 6:
+                            c = SigCar(ref i);
+                            if ((lenguaje = "0123456789").IndexOf(c) >= 0)
+                                _edoAct = 7; else
+                            {
+                                i = iniToken;
+                                return false;
+                            } break;
+                        case 7: c = SigCar(ref i);
+                            if ((lenguaje = "0123456789").IndexOf(c) >= 0)
+                                _edoAct = 7;
+                            else if ((lenguaje = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_'abcdefghijklmnopqr").IndexOf(c) >= 0)
+                                _edoAct = 8; else
+                            {
+                                i = iniToken;
+                                return false;
+                            }break;
+
+                        case 8: i--;
+                            return true;
+                            break;
+                        case 9: c = SigCar(ref i);
+                            if ((lenguaje = "=").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = ";").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = ",").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = ".").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = "+").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = "_").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = "*").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = "/").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = "(").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else if ((lenguaje = ")").IndexOf(c) >= 0)
+                                _edoAct = 10;
+                            else { i = iniToken;return false; }
+                            break;
+                        case 10: return true;
+                            break;
+                        case 11: c = SigCar(ref i);
+                            if ((lenguaje = "\"").IndexOf(c) >= 0)
+                                _edoAct = 12;
+                            else { i = iniToken;
+                                return false;
+                            }break;
+                        case 12: c = SigCar(ref i);
+                            if ((lenguaje = "\"").IndexOf(c) >= 0)
+                                _edoAct = 13;
+                            else if ((lenguaje = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_'abcdefghijklmnopqrstuvwxyz").IndexOf(c) >= 0)
+                                _edoAct = 12;
+                            else { i = iniToken;
+                                return false;
+                            }break;
+                        case 13: return true;
+                            break;
+
+                          
+
+
+
+
+
+
+
+                    }
+                switch(_edoAct)
+                {
+                    case 2:
+                    case 5:
+                    case 8:
+                        --i;
+                        return true;
+                }
+                return false;
+                        
+
+            }//Fin de clase Automata.
+        } class Lexico
+        {
+            const int Tokenc = 5;
+            const int maxtokens = 500;
+            string[] _lexemas;
+            string[] _tokens;
+            string _lexema;
+            int _notokens;
+            int _i;
+            int _initoken;
+            Automata oAFD;
+            public Lexico()
+            {
+                _lexemas = new string[maxtokens];
+                _tokens = new string[maxtokens];
+                oAFD = new Automata();
+                _i = 0;
+                _initoken = 0;
+                _notokens = 0;
+            }
+            public int Notokens
+            {
+                get { return _notokens; }
+            }
+            public string [] Lexema
+            {
+                get { return _lexemas; }
+            }
+
+            public string [] Token
+            {
+                get { return _tokens; }
+            }
+            public void Inicia()
+            {
+                _i = 0;
+                _initoken = 0;
+                _notokens = 0;
+
+            }
+            public void Analiza(string texto)
+            {
+                bool recAuto;
+                int noAuto;
+                while(_i< texto.Length)
+                {
+                    recAuto = false;
+                    noAuto = 0;
+                    for (; noAuto < Tokenc && !recAuto;)
+                        if (oAFD.Reconoce(texto, _initoken, ref _i, noAuto))
+                            recAuto = true;
+                        else noAuto++;
+                    if (recAuto)
+                    {
+                        _lexema = texto.Substring(_initoken, _i - _initoken);
+                        switch (noAuto)
+                        {
+                            case 0:
+                                break;
+                            case 1:
+                                if (EsId())
+                                    _tokens[_notokens] = "id";
+                                else _tokens[_notokens] = _lexema;
+                                break;
+                            case 2:
+                                _tokens[_notokens] = "num";
+                                break;
+                            case 3:
+                                _tokens[_notokens] = _lexema;
+                                break;
+                            case 4:
+                                _tokens[_notokens] = "cad";
+                                break;
+
+                        }
+                        if (noAuto != 0)
+                            _lexemas[_notokens++] = _lexema;
+
+                    }
+                    else _i++;
+                    _initoken = _i;
+                       
+                }
+            }
+            private bool EsId()
+            {
+                string[] palres = { "inicio", "fin", "const", "var", "entero", "real", "cadena", "leer", "visua" };
+                for (int i = 0; i < palres.Length; i++)
+                    if (_lexema == palres[i])
+                        return false;
+                return true;
+            }
+
+                
+
+        }//Fin clase lexico
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Analiza_Lexico.Inicia();
+            Analiza_Lexico.Analiza(textBox1.Text);
+            dataGridView1.Rows.Clear();
+            if (Analiza_Lexico.Notokens > 0)
+                dataGridView1.Rows.Add(Analiza_Lexico.Notokens);
+            for(int i =0; i<Analiza_Lexico.Notokens;i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = Analiza_Lexico.Token[i];
+                dataGridView1.Rows[i].Cells[1].Value = Analiza_Lexico.Lexema[i];
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            textBox1.Text = " ";
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
